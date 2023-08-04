b6592843f2be6af98174f5da8e5dce4c7655e485
inathan44 <inathan44@yahoo.com>
2023-08-04 12:18:14 -0400

    Routes and sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+// import Hero from './hero/Hero';
+import Footer from './components/Footer';
+import Homepage from './Homepage';
 
 function App() {
-  return <></>;
+  return (
+    <div className='font-inter'>
+      <Navbar />
+      <Routes>
+        {/* <Route path='/' element={<Hero />} /> */}
+        <Route path='/' element={<Homepage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
