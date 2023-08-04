095288f03e7369ac3ac838a958616a72006e7d10
inathan44 <inathan44@yahoo.com>
2023-08-04 12:17:37 -0400

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: 'Inter, sans-serif',
+      },
+      colors: {
+        'off-black': '#3E3E3E',
+        'logo-red': '#ff0c65',
+      },
+    },
   },
   plugins: [],
 };
