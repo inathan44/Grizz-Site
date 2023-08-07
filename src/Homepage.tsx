ccf914f94b92373b47c2a1eb93f0aa1bf4473c84
inathan44 <inathan44@yahoo.com>
2023-08-07 17:12:22 -0400

    minor text changes

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -5,7 +5,7 @@ const Homepage = () => {
     <div className='min-h-[calc(100vh_-_122px)] mt-12'>
       <div className='flex flex-col items-center gap-2'>
         <img src={logo} alt='Grizz logo' className='w-48 md:w-40' />
-        <small className='text-sm'>prospect with confidence.</small>
+        <small className='text-sm'>prospect with confidence</small>
       </div>
       <div className='text-center pt-24 px-4'>
         <div className='text-lg'>
@@ -15,16 +15,16 @@ const Homepage = () => {
           </h2>
         </div>
         <div className='pt-16'>
-          <p className='py-6'>How it works</p>
+          <p className='py-6 underline'>How it works</p>
           <ol className='flex flex-col gap-2'>
             <li>
               {`1)`} Type in a technology that is important for your prospects
-              to have.
+              to have
             </li>
             <li>
               {`2)`} Grizz will dissect job boards and populate organizations
               that are hiring for positions where knowledge of that technology
-              is necessary.
+              is necessary
             </li>
             <li>{`3)`} Prospect!</li>
           </ol>
